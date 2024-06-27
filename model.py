ca536a38c2313891a52524e81baf28747a9702c8
TIN NGUYEN <130662495+nguyenthanhtin2810@users.noreply.github.com>
2024-06-27 23:41:25 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torchvision.models import resnet50, ResNet50_Weights
 
 class AgeGenderCNN(nn.Module):
     def __init__(self, num_classes=2):
@@ -43,6 +44,47 @@ class AgeGenderCNN(nn.Module):
         x = self.conv3(x)
         x = self.conv4(x)
         x = x.view(x.size(0), -1)
+        reg_output = self.reg_fc(x)
+        cls_output = self.cls_fc(x)
+        return reg_output, cls_output
+
+class AgeGenderResNet50(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.backbone = resnet50(weights=ResNet50_Weights.DEFAULT)
+        del self.backbone.fc
+
+        for name, param in self.backbone.named_parameters():
+            if "layer3" not in name and "layer4" not in name:
+                param.requires_grad = False
+
+        self.reg_fc = nn.Sequential(
+            nn.Linear(2048, 1024),
+            nn.ReLU(True),
+            nn.Dropout(p=0.5),
+            nn.Linear(1024, 1)
+        )
+        self.cls_fc = nn.Sequential(
+            nn.Linear(2048, 1024),
+            nn.ReLU(True),
+            nn.Dropout(p=0.5),
+            nn.Linear(1024, 2)
+        )
+
+
+    def forward(self, x):
+        x = self.backbone.conv1(x)
+        x = self.backbone.bn1(x)
+        x = self.backbone.relu(x)
+        x = self.backbone.maxpool(x)
+
+        x = self.backbone.layer1(x)
+        x = self.backbone.layer2(x)
+        x = self.backbone.layer3(x)
+        x = self.backbone.layer4(x)
+
+        x = self.backbone.avgpool(x)
+        x = torch.flatten(x, 1)
         reg_output = self.reg_fc(x)
         cls_output = self.cls_fc(x)
         return reg_output, cls_output
